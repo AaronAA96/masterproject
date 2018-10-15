a6a284db196c20a83db988e8e21faeea92e51de9
TheLifeObtainer <aaronellis1@hotmail.co.uk>
2018-10-15 15:31:54 +0100

    15th Oct Check Book --- CSS -- PUG

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const app = express()
 const port = 3000
 app.set('view engine', 'pug')
 
-//Import the mongoose module
-var mongoose = require('mongoose');
+// //Import the mongoose module
+// var mongoose = require('mongoose');
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-var db = mongoose.connection;
+// //Set up default mongoose connection
+// var mongoDB = 'mongodb://127.0.0.1/my_database';
+// mongoose.connect(mongoDB);
+// // Get Mongoose to use the global promise library
+// mongoose.Promise = global.Promise;
+// //Get the default connection
+// var db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hallam Banking', message: 'Welcome to Hallam Banking!' })
